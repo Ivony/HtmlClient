bc2ee8829151a72d5e8c5269f1e70a030afd6ffe
Ivony <Ivony@live.com>
2013-11-13 18:26:59 +0800

    启用 NuGet 包还原

--- Ivony.Html.Client/HtmlSiteDownloader.cs
+++ Ivony.Html.Client/HtmlSiteDownloader.cs
@@ -11,9 +11,9 @@ namespace Ivony.Html.Client
   public class HtmlSiteDownloader : IDisposable
   {
 
-    public HtmlSiteDownloader() : this( new HtmlClient() ) { }
+    public HtmlSiteDownloader() : this(new HtmlClient()) { }
 
-    public HtmlSiteDownloader( HtmlClient client )
+    public HtmlSiteDownloader(HtmlClient client)
     {
       Client = client;
     }
@@ -31,58 +31,65 @@ namespace Ivony.Html.Client
 
     private object sync = new object();
 
-    public Task<IHtmlDocument[]> RetrieveAllPages( string entryUri )
+    public IHtmlDocument[] RetrieveAllPages(string entryUri)
     {
-      return RetrieveAllPagesCore( new Uri( entryUri ) );
+      return RetrieveAllPages(new Uri(entryUri));
     }
 
 
-    public Task<IHtmlDocument[]> RetrieveAllPages( Uri entryUri )
+    public IHtmlDocument[] RetrieveAllPages(Uri entryUri)
     {
-      return RetrieveAllPagesCore( entryUri );
+      var task = RetrieveAllPagesCore(entryUri);
+      task.RunSynchronously();
+      return task.Result;
     }
 
 
-    protected async Task<IHtmlDocument[]> RetrieveAllPagesCore( Uri entryUri )
+    protected async Task<IHtmlDocument[]> RetrieveAllPagesCore(Uri entryUri)
     {
 
-      var document = await Client.Get( entryUri );
-      if ( document == null )
+      var task = Client.Get(entryUri);
+      task.Wait();
+      if (task.IsFaulted)
+        throw new Exception();
+      var document = task.Result;
+      if (document == null)
         return new IHtmlDocument[0];
 
-      lock ( sync )
+      lock (sync)
       {
-        siteLinks.UnionWith( FindLinks( document ) );
+        siteLinks.UnionWith(FindLinks(document));
       }
 
-      if ( IsObjective( document ) )
-        Save( document );
+      if (IsObjective(document))
+        Save(document);
 
 
       List<IHtmlDocument> result = new List<IHtmlDocument>();
 
       string[] allLinks;
-      lock ( sync )
+      lock (sync)
       {
-        allLinks = siteLinks.Except( tracedLinks ).ToArray();
+        allLinks = siteLinks.Except(tracedLinks).ToArray();
       }
 
 
-      foreach ( var link in allLinks )
+      foreach (var link in allLinks)
       {
-        var traceUri = new Uri( entryUri, link );
-        if ( Tracable( entryUri, traceUri ) )
+        var traceUri = new Uri(entryUri, link);
+        if (Tracable(entryUri, traceUri))
         {
-          lock ( sync )
+          lock (sync)
           {
             var traceLink = traceUri.AbsoluteUri;
-            if ( tracedLinks.Contains( traceLink ) )
+            if (tracedLinks.Contains(traceLink))
               continue;
 
-            tracedLinks.Add( traceLink );
+            tracedLinks.Add(traceLink);
           }
 
-          result.AddRange( await RetrieveAllPagesCore( traceUri ) );
+          RetrieveAllPagesCore(traceUri);
+          result.AddRange(await );
         }
       }
 
@@ -96,7 +103,7 @@ namespace Ivony.Html.Client
     /// </summary>
     /// <param name="document">要判断的文档</param>
     /// <returns>是否为要保存的目标文档</returns>
-    protected virtual bool IsObjective( IHtmlDocument document )
+    protected virtual bool IsObjective(IHtmlDocument document)
     {
       return true;
     }
@@ -106,12 +113,12 @@ namespace Ivony.Html.Client
     /// 保存指定文档
     /// </summary>
     /// <param name="document">要保存的文档</param>
-    protected virtual void Save( IHtmlDocument document )
+    protected virtual void Save(IHtmlDocument document)
     {
-      var filename = Guid.NewGuid().ToString( "N" ) + ".html";
-      using ( var stream = File.OpenWrite( Path.Combine( @"C:\Temp", filename ) ) )
+      var filename = Guid.NewGuid().ToString("N") + ".html";
+      using (var stream = File.OpenWrite(Path.Combine(@"C:\Temp", filename)))
       {
-        document.Render( stream, Encoding.UTF8 );
+        document.Render(stream, Encoding.UTF8);
       }
     }
 
@@ -122,19 +129,19 @@ namespace Ivony.Html.Client
     /// <param name="refer">引用来源</param>
     /// <param name="traceUri">需要判断的 URI 地址</param>
     /// <returns>是否可以进行追踪</returns>
-    protected virtual bool Tracable( Uri refer, Uri traceUri )
+    protected virtual bool Tracable(Uri refer, Uri traceUri)
     {
       return refer.Host == traceUri.Host;
     }
 
-    protected virtual IEnumerable<string> FindLinks( IHtmlDocument document )
+    protected virtual IEnumerable<string> FindLinks(IHtmlDocument document)
     {
 
       document.ResolveUriToAbsoluate();
 
-      foreach ( var href in document.Find( "a[href]" ).Select( element => element.Attribute( "href" ).Value() ) )
+      foreach (var href in document.Find("a[href]").Select(element => element.Attribute("href").Value()))
       {
-        var uri = new UriBuilder( new Uri( document.DocumentUri, href ) );
+        var uri = new UriBuilder(new Uri(document.DocumentUri, href));
         uri.Fragment = null;
         yield return uri.Uri.AbsoluteUri;
       }
